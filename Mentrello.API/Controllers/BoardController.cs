1e6b40af2ec65676e64876883c32475f87653ad1
Vasyl Kamyshnikov <vasyl_kamyshnikov@epam.com>
2020-11-15 16:12:15 +0200

    New solution with skeleton.

---
+++ Mentrello.API/Controllers/BoardController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Mentrello.API.Controllers
+{
+    [ApiController]
+    [Route("board")]
+    public class BoardController : ControllerBase
+    {
+        [HttpPost]
+        [Route("create", Name = "CreateBoard")]
+        public async Task<IActionResult> CreateBoard()
+        {
+            return Ok("New BOARD has been created!");
+        }
+
+        [HttpGet]
+        [Route("all", Name = "GetBoardCollection")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok("Here is all available BOARDs!");
+        }
+
+        [HttpGet]
+        [Route("{id:guid}", Name = "GetBoardById")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok($"This is BOARD #'{id}'!");
+        }
+
+        [HttpPatch]
+        [Route("update/{id:guid}", Name = "UpdateBoard")]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            return Ok($"BOARD # {id} has been updated!");
+        }
+
+        [HttpDelete]
+        [Route("delete/{id:guid}", Name = "DeleteBoard")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok($"BOARD # {id} has been deleted!");
+        }
+    }
+
+}
---
+++ Mentrello.API/Controllers/CardController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Mentrello.API.Controllers
+{
+    [ApiController]
+    [Route("card")]
+    public class CardController : ControllerBase
+    {
+        [HttpPost]
+        [Route("create", Name = "CreateCard")]
+        public async Task<IActionResult> CreateCard()
+        {
+            return Ok("New CARD has been created!");
+        }
+
+        [HttpGet]
+        [Route("all", Name = "GetCardCollection")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok("Here is all available CARDs!");
+        }
+
+        [HttpGet]
+        [Route("{id:guid}", Name = "GetCardById")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok($"This is CARD #'{id}'!");
+        }
+
+        [HttpPatch]
+        [Route("update/{id:guid}", Name = "UpdateCard")]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            return Ok($"CARD # {id} has been updated!");
+        }
+
+        [HttpDelete]
+        [Route("delete/{id:guid}", Name = "DeleteCard")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok($"CARD # {id} has been deleted!");
+        }
+    }
+
+}
